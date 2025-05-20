ce79a47b857d2f6b83cce4591b0a11d64581a0e5
Alberto-LL <Lautaroalbertolier@gmail.com>
2025-05-20 19:35:11 -0300

    CRUD simple Localidades

---
+++ src/app.js
@@ -0,0 +1,67 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { parse } from 'path';
+
+
+// Se creo la instancia de express donde se crean los endpoints
+const app = express();
+
+
+let Localidades =[];
+
+
+// mostrar todas las localidades
+
+app.get('/api/getLocations', (req, res) => {
+    res.json(Localidades);
+});
+
+// Alta de Localidades
+app.post('/api/addLocation', express.json(), (req, res) => {
+    const body = req.body;
+    const newLocation = {
+        id : Localidades.length + 1,
+        nombre: body.nombre,
+        codigoPostal: body.codigoPostal,
+        provincia: body.provincia,
+    }
+    Localidades.push(newLocation);
+    res.status(201).json(newLocation);
+});
+
+// Baja de Localidade
+app.delete('/api/deleteLocation/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = Localidades.findIndex(location => location.id === id);
+    const location = Localidades[index];
+    if (index !== -1) {
+        Localidades.splice(index, 1);
+        res.status(200).json(location);
+    } else {
+        res.status(404).json({ message: 'Location not found' });
+    }
+});
+
+// Modificacion de Localidades
+app.patch('/api/updateLocation/:id', express.json(), (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = Localidades.findIndex(location => location.id === id);
+    const location = Localidades[index];
+    if (index !== -1){
+        const updatedLocation = {
+            id: location.id,
+            nombre: req.body.nombre || location.nombre,
+            codigoPostal: req.body.codigoPostal || location.codigoPostal,
+            provincia: req.body.provincia || location.provincia,
+        };
+        Localidades[index] = updatedLocation;
+        res.status(200).json(updatedLocation);
+    }
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
