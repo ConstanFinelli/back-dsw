f418bf02ba1f23b81135aeeba88648e6640a19d3
ConstanFinelli <constanfinelli@gmail.com>
2025-05-20 21:21:03 -0300

    Merge added, deleteCoupon and getCoupon

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 import express from 'express';
 
 const app = express();
-
 app.use(express.json())
 
-app.listen('3000', () =>{console.log("Server running on port 3000")})
 
 let coupons = [
     {id: 1, discount: 0.20, expiringDate: new Date(), status: 'Active' },
 ]
+let Localidades =[];
 
+// mostrar todos los cupones
 app.get('/api/getCoupons', (req, res)=>{
     res.json(coupons)
 })
 
+// mostrar un cupón por id
+app.get('/api/getCoupon/:id', (req, res)=>{
+    const coupon = coupons.find((c) => c.id == req.params.id)
+    res.json(coupon)
+})
+
+// añadir cupón
 app.post('/api/addCoupon', (req, res)=>{
     const coupon = {
         id: coupons.length + 1,
@@ -23,6 +30,7 @@ app.post('/api/addCoupon', (req, res)=>{
     res.status(201).send({message:"Coupon added succesfully", data:coupon})
 })
 
+// actualizar cupón
 app.patch('/api/updateCoupon/:id', (req, res) =>{
     let couponIdx = coupons.findIndex((coupon) => coupon.id == req.params.id)
     if (couponIdx == -1){
@@ -37,4 +45,72 @@ app.patch('/api/updateCoupon/:id', (req, res) =>{
     res.status(201).send({message:"Coupon updated succesfully", data:coupons[couponIdx]})
     }
 })
+
+
+// eliminar cupón
+app.delete('/api/deleteCoupon/:id', (req, res) =>{
+    let couponIdx = coupons.findIndex((coupon) => coupon.id == req.params.id)
+    if (couponIdx == -1){
+        res.status(404).send({message:"Coupon not found"})
+    }else{
+    let deletedCoupon = coupons[couponIdx]
+    coupons.splice(couponIdx, 1)
+    res.status(201).send({message:"Coupon deleted succesfully", deleted:deletedCoupon})
+    }
+})
+
+// mostrar todas las localidades
+app.get('/api/getLocations', (req, res) => {
+    res.json(Localidades);
+});
+
+// Alta de Localidades
+app.post('/api/addLocation', express.json(), (req, res) => {
+    const body = req.body;
+    const newLocation = {
+        id : Localidades.length + 1,
+        nombre: body.nombre,
+        codigoPostal: body.codigoPostal,
+        provincia: body.provincia,
+    }
+    Localidades.push(newLocation);
+    res.status(201).json(newLocation);
+});
+
+// Baja de Localidade
+app.delete('/api/deleteLocation/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = Localidades.findIndex(location => location.id === id);
+    const location = Localidades[index];
+    if (index !== -1) {
+        Localidades.splice(index, 1);
+        res.status(200).json(location);
+    } else {
+        res.status(404).json({ message: 'Location not found' });
+    }
+});
+
+// Modificacion de Localidades
+app.patch('/api/updateLocation/:id', express.json(), (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = Localidades.findIndex(location => location.id === id);
+    const location = Localidades[index];
+    if (index !== -1){
+        const updatedLocation = {
+            id: location.id,
+            nombre: req.body.nombre || location.nombre,
+            codigoPostal: req.body.codigoPostal || location.codigoPostal,
+            provincia: req.body.provincia || location.provincia,
+        };
+        Localidades[index] = updatedLocation;
+        res.status(200).json(updatedLocation);
+    }
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 
