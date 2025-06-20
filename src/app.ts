d3f43bf035c474189364008dcf549b3dab17c469
lucianoacosta23 <lucianoacostautn@gmail.com>
2025-06-20 15:58:34 -0300

    app.ts update

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import {couponRouter} from './coupon/coupon.route.js'
 import {localityRouter} from './locality/locality.route.js';
+import {categoryrouter} from './category/category.route.js';
+
 const app = express();
 app.use(express.json())
 
-
+app.use('/api/category', categoryrouter) ;
 app.use('/api/coupons', couponRouter);
 app.use('/api/localities', localityRouter);
 
