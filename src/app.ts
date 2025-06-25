30f6a62b1b4dc3b792e3c6bd4f228fd15ce1ad7e
Constantino Finelli <constantinofinelli@Constantinos-MacBook-Air.local>
2025-06-25 16:26:34 -0300

    minor fix in app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ dotenv.config();
 import express from 'express';
 import {couponRouter} from './coupon/coupon.route.js'
 import {localityRouter} from './locality/locality.route.js';
-import {categoryrouter} from './category/category.route.js';
+import {categoryRouter} from './category/category.route.js';
 
 
 
@@ -13,7 +13,7 @@ import {categoryrouter} from './category/category.route.js';
 const app = express();
 app.use(express.json())
 
-app.use('/api/category', categoryrouter) ;
+app.use('/api/category', categoryRouter) ;
 app.use('/api/coupons', couponRouter);
 app.use('/api/localities', localityRouter);
 
