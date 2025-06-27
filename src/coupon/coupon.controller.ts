223cdf1ac4caa094e94f1cf3ec45c43d3d5aaef6
ConstanFinelli <constanfinelli@gmail.com>
2025-06-26 21:47:05 -0300

    Solved Typescript strongly-typed violation in CRUD Coupon

--- src/coupon/coupon.controller.ts
+++ src/coupon/coupon.controller.ts
@@ -17,32 +17,35 @@ async function findAll(req:Request, res:Response){
 async function findOne(req:Request, res:Response){
     const coupon = await repository.findOne(Number(req.params.id))
     if(!coupon){
-        return res.status(404).send({message:"Coupon not found"}) as any
+        res.status(404).send({message:"Coupon not found"})
+        return
     }
-    return res.send({data:coupon}) as any
+    res.send({data:coupon})
     
 }
 
 async function add(req:Request, res:Response){
     const input = req.body.sanitizedInput
     if (!input.discount || !input.expiringDate || !input.status) {
-        return res.status(400).send({ message: "Missing required fields" }) as any
+        res.status(400).send({ message: "Missing required fields" })
+        return
     }
     const coupon = new Coupon(0, input.discount, new Date(), input.status)
     try{
     const newCoupon = await repository.add(coupon)
-    return res.status(201).send({message:"Coupon created succesfully", data:newCoupon}) as any
+    res.status(201).send({message:"Coupon created succesfully", data:newCoupon})
     }catch(e){
-        return res.send({message:e}) as any
+        res.send({message:e })
     }
 }
 
 async function remove(req:Request, res:Response){
     const deletedCoupon = await repository.remove(Number(req.params.id))
     if(!deletedCoupon){
-        return res.status(404).send({message:"Coupon not found"}) as any
+        res.status(404).send({message:"Coupon not found"})
+        return
     }
-    return res.status(201).send({message:"Coupon deleted succesfully", data:deletedCoupon}) as any
+    res.status(201).send({message:"Coupon deleted succesfully", data:deletedCoupon})
     
 }
 
@@ -50,14 +53,16 @@ async function update(req:Request, res:Response){
     const input = req.body.sanitizedInput
     const id = req.params.id
     if (!input.discount || !input.expiringDate || !input.status) {
-        return res.status(400).send({ message: "Missing required fields" }) as any
+        res.status(400).send({ message: "Missing required fields" })
+        return
     }
     const newCoupon = new Coupon(Number(id), input.discount, input.expiringDate, input.status)
     const updated = await repository.update(newCoupon)
     if (!updated){
-       return res.status(404).send({message:"Coupon not found"}) as any
+       res.status(404).send({message:"Coupon not found"})
+       return
     }
-    return res.status(201).send({message:"Coupon updated succesfully", coupon:updated}) as any
+    res.status(201).send({message:"Coupon updated succesfully", coupon:updated})
 }
 
 function sanitizeCouponInput(req:Request, res:Response, next:NextFunction){
