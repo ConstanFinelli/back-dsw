cbbccf9499c129f10b6e7d75e14ea20b3f389aac
ConstanFinelli <constanfinelli@gmail.com>
2025-07-16 15:55:27 -0300

    CRUD Cupones converted to MikroORM

--- src/coupon/coupon.controller.ts
+++ src/coupon/coupon.controller.ts
@@ -15,19 +15,39 @@ async function findAll(req:Request, res:Response){
 }
 
 async function findOne(req:Request, res:Response){
-    
+    try{
+        const coupon = await repository.findOne(Number.parseInt(req.params.id))
+        res.send({data:coupon})
+    }catch(e){
+        res.send({error:e})
+    }
 }
 
 async function add(req:Request, res:Response){
-    
+    try{
+        const coupon = await repository.add(req.body.sanitizedInput)
+        res.send({data:coupon})
+    }catch(e:any){
+        res.send({error:e})
+    }
 }
 
 async function remove(req:Request, res:Response){
-    
+    try{
+        const coupon = await repository.remove(Number.parseInt(req.params.id))
+        res.send({removedCoupon:coupon})
+    }catch(e:any){
+        res.send({error:e})
+    }
 }
 
 async function update(req:Request, res:Response){
-    
+    try{
+        const coupon = await repository.update(Number.parseInt(req.params.id) ,req.body.sanitizedInput)
+        res.send({updatedCoupon:coupon})
+    }catch(e:any){
+        res.send({error:e})
+    }
 }
 
 function sanitizeCouponInput(req:Request, res:Response, next:NextFunction){
