239213da2e21da6d94202ca6214b43e0f918c3d8
Alberto-ll <Lautaroalbertolier@gmail.com>
2025-08-01 15:49:45 -0300

    old folder deleted

--- old/category.router.js
+++
@@ -1,9 +0,0 @@
-const router = Router();
-
-router.post('/', createCategory);
-router.get('/', getAllCategories);
-router.get('/:id', getCategoryById);
-router.put('/:id', updateCategory);
-router.delete('/:id', deleteCategory);
-
-export default router;
--- old/coupon.repository.js
+++
@@ -1,3 +0,0 @@
-export let coupons = [
-    {id: 1, discount: 0.20, expiringDate: new Date(), status: 'Active' },
-]
