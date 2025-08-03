53f7d544d4b12355f03e8646ce62f0d6175be594
Alberto-ll <Lautaroalbertolier@gmail.com>
2025-08-03 12:26:18 -0300

    new entity added

--- src/business/business.entities.ts
+++ src/business/business.entities.ts
@@ -1,11 +1,29 @@
-import { Entity, ManyToOne, OneToMany, PrimaryKey, Property } from '@mikro-orm/core';
+import { Entity, ManyToOne, OneToMany, OneToOne, PrimaryKey, Property } from '@mikro-orm/core';
 import { Locality } from '../locality/locality.entities';
+import { User } from '../user/user.entities';
 
 @Entity()
 export class Business {
   @PrimaryKey()
   id!: number;
 
+  // relacion del dueño con el negocio
+  @OneToOne(() => User, { owner: true })
+  owner!: User;
+
   @ManyToOne(() => Locality)
   locality!: Locality;
+
+  @Property()
+  businessName!: string;
+
+  @Property()
+  address!: string;
+
+  @Property()
+  averageRating!: number;
+
+  @Property()
+  reservationDepositPercentage!: number;
+
 }
